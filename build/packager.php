568f704c0ebf5355823b04bdb31ff0ec3e4eac67
Felipe César Silveira de Assis <fel.cesar@gmail.com>
2018-07-03 17:40:37 -0300

    Updated scripts that generate new phar file

--- build/packager.php
+++ build/packager.php
@@ -16,25 +16,14 @@ $stageDirectory = __DIR__ . '/staging';
 $projectRoot = __DIR__ . '/../';
 
 
-# STEP 1 - Basic configurations
+#Basic configurations
 $packager = new \Burgomaster($stageDirectory, $projectRoot);
 
-# STEP 2 - Copying basic files
-// Copy basic files to the stage directory. Note that we have chdir'd onto
-// the $projectRoot directory, so use relative paths.
-// $basicFiles = ['lib/guzzle.phar'/*'README.md', 'LICENSE'*/];
-// foreach ($basicFiles as $file) {
-//     $packager->deepCopy($file, $file);
-// }
-
-# STEP 3 - Copying the source files (again relative to project root)
-// Copy each dependency to the staging directory. Copy *.php files.
-//$packager->recursiveCopy('src', '', ['php']);
+#Copying the source files (again relative to project root)
 $packager->recursiveCopy('src', 'CloudRail', ['php']);
 
-# STEP 4 - Map all the classes that the Phar will let available
-// Create the classmap autoloader, and instruct the autoloader to
-// automatically require the 'GuzzleHttp/functions.php' script.
+#Map all the classes that the Phar will let available
+
 $explicitFiles = [];//['GuzzleHttp/functions.php'];
 $packager->createAutoloader($explicitFiles);
 
@@ -46,24 +35,8 @@ fwrite($h, <<<EOT
 EOT
 );
 
-# STEP 5 - Generate the Phar - WARNING BE CAREFUL WHEN CHANGING NAMES FROM HERE
+#Generate the Phar - WARNING BE CAREFUL WHEN CHANGING NAMES FROM HERE
 $targetDirectory = $productDir;
 $pharPath = $targetDirectory . '/CloudRail.phar';
 // Create a phar file from the staging directory at a specific location
 $packager->createPhar($pharPath);
-
-//Generating DEV autoloader
-$packager->createAutoloaderDev($explicitFiles,"autoloaderDev.php", __DIR__ . "/..");
-
-// Create a zip file from the product directory
-$unconpressedProduct = $productDir . "/CloudRail-php";
-mkdir($unconpressedProduct);
-$output = shell_exec("mv  $pharPath $unconpressedProduct");
-
-$libPath = $unconpressedProduct . "/lib";
-if (!file_exists($libPath)){
-    mkdir($libPath);
-}
-$dependecies = __DIR__ . "/../lib";
-$output = shell_exec("cp -r $dependecies $unconpressedProduct");
-$output = shell_exec("cd $productDir && zip -r3 ./CloudRail-php.zip ./CloudRail-php");
